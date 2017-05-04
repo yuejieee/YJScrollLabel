a874f8f8f007f0052d38708d6d1114bb6eaeaef4
yuejieee <yuejieee@gmail.com>
2017-05-04 11:45:48 +0800

    Add PreviewImage

--- YJScrollLabel/ViewController.swift
+++ YJScrollLabel/ViewController.swift
@@ -14,12 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         
+        let titleRect = CGRect.init(x: 0, y: 0, width: 300, height: 40)
+        let titleLabel = UILabel.init(frame: titleRect)
+        titleLabel.center = CGPoint.init(x: self.view.center.x, y: 200)
+        self.view.addSubview(titleLabel)
+        titleLabel.text = "YJScrollLabel"
+        titleLabel.font = UIFont.systemFont(ofSize: 30, weight: 2)
+        titleLabel.textAlignment = .center
+        
         let rect = CGRect.init(x: 0, y: 0, width: 250, height: 30)
         let scrollLabel = ScrollLabel.init(frame: rect)
         scrollLabel.center = self.view.center
         self.view.addSubview(scrollLabel)
-        scrollLabel.setTitle("test😀😁🤣😂😄😅😆😇😉😊🙂😊🙂🙃☺️😋😌😍😘test")
-        scrollLabel.backgroundColor = UIColor.lightGray
+        scrollLabel.setTitle("😀😁🤣😂😄😅😆😇😉😊🙂😊🙂🙃☺️😋😌😍😘")
+        scrollLabel.backgroundColor = UIColor.init(white: 0, alpha: 0.8)
     }
 
     override func didReceiveMemoryWarning() {
